539120ef02408c7700e829d6b914cde46cb8ace2
Hamed Nemati <hitech.innovative@gmail.com>
2024-02-15 17:00:55 +0100

    Add live grep keymap

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -33,6 +33,7 @@ require('telescope').load_extension('fzy_native')
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
